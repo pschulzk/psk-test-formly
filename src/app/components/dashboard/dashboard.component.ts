5eddbca3d34c4a121fbe09fb370bb65b6c48e873
Philip Schulz-Klingauf <psk@schulz-klingauf.de>
2023-11-04 14:52:00 +0100

    feat: integrate formly

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormlyFieldConfig, FormlyModule } from '@ngx-formly/core';
+import { FormlyPrimeNGModule } from '@ngx-formly/primeng';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'psk-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    ButtonModule,
+    CommonModule,
+    FormlyModule,
+    FormlyPrimeNGModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  formGroup = new FormGroup({});
+  model = { email: 'email@gmail.com' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'email',
+      type: 'input',
+      props: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true,
+      }
+    }
+  ];
+
+  onSubmit(model: any) {
+    console.log(model);
+  }
+}
